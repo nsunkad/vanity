5cca28fa360ebb775bdbc106d559be01c60c2375
nsunkad <nsunkad2@illinois.edu>
2024-04-27 04:37:31 -0500

    improve trigger code by removing nested ifs

---
+++ vanity/backend/api/MaxItemsPerBagTrig.sql
@@ -0,0 +1,11 @@
+
+DELIMITER //
+CREATE TRIGGER MaxItemsPerBagTrig 
+    BEFORE INSERT ON BagItems
+    FOR EACH ROW 
+    BEGIN 
+        SET @num_items = (SELECT Account.NumItemsPerUser FROM (SELECT UserId, COUNT(*) AS NumItemsPerUser FROM BagItems GROUP BY UserId) Account WHERE Account.UserId = new.UserId); 
+        IF @num_items IS NOT NULL AND @num_items = 20 
+            THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User already has max 20 items in their bag'; 
+        END IF; 
+    END//
