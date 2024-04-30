5b7c2f32cd21b96ae82f3810f8c9aca419e68c11
julielima8 <julie11@illinois.edu>
2024-04-30 16:24:13 -0500

    remove unused variables in welcome page

--- vanity/frontend/src/pages/Welcome/WelcomePage.js
+++ vanity/frontend/src/pages/Welcome/WelcomePage.js
@@ -3,16 +3,6 @@ import './WelcomePage.css'; // Assuming your CSS file is named WelcomePage.css
 import {LogInButton, SignUpButton} from '../../components/general/Buttons.js';
 
 function WelcomePage() {
-  // You might use a routing solution like React Router for navigation
-  // For demonstration purposes, we're using simple callbacks
-  const navigateToLogin = () => {
-    // Navigation logic to LogInPage
-  };
-
-  const navigateToSignUp = () => {
-    // Navigation logic to SignUpPage
-  };
-
   return (
     <div className="welcome-container">
       <h1 className="welcome-title">vanity.</h1>
