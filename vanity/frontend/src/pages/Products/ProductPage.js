7fb01777e77eec3095419958a819df962caeb06e
Oju Chaudhary <ojaswee2@illinois.edu>
2024-04-30 21:50:50 -0500

    product info page working except reviews

--- vanity/frontend/src/pages/Products/ProductPage.js
+++ vanity/frontend/src/pages/Products/ProductPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faStar, faTag } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faStar, faTag, faPlus } from '@fortawesome/free-solid-svg-icons';
 import './ProductPage.css';
 
 function ProductPage() {
@@ -45,34 +45,46 @@ function ProductPage() {
 
     return (
         <div className="product-page">
-            <h1>{productInfo.productName || productName}</h1>
-            <div className="brand-name">{productInfo.brandName}</div>
-            {productInfo.isPopular && <p className="popular-product">This is a popular product!</p>}
-            <div>
-                <span className="price"><FontAwesomeIcon icon={faTag} /> Price: {productInfo.price}</span>
-                <span className="likes"><FontAwesomeIcon icon={faHeart} /> Likes: {productInfo.likeCount}</span>
-                <span className="rating"><FontAwesomeIcon icon={faStar} /> Rating: {productInfo.avgRating} ({productInfo.totalNumReviews} reviews)</span>
+            <div className="product-outer">
+                <div className="product-left">
+                    <h1>{productInfo.productName || productName}</h1>
+                    <div className="brand-name">{productInfo.brandName}</div>
+                    <div className="product-attributes-smaller">
+                        <span className="price"><FontAwesomeIcon icon={faTag} /> ${productInfo.price}</span>                    
+                        <span className="likes"><FontAwesomeIcon icon={faHeart} /> {productInfo.likeCount}</span>
+                        <span className="rating">
+                            {[...Array(Math.ceil(productInfo.avgRating))].map((_, index) => (
+                                <FontAwesomeIcon key={index} icon={faStar} />
+                            ))} {productInfo.avgRating}
+                        </span>
+                    </div>
+                    {productInfo.isPopular && <p className="popular-product">This is a popular product!</p>}
+                </div>
+                <div className="product-right">
+                    <div className="add-to-bag-outer">
+                        <div className="add-to-bag-inner">
+                            <FontAwesomeIcon icon={faPlus} size="5x" />
+                            <div className="button-text">add to my bag</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>name</th>
-                        <th>rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {productInfo.usersAlsoBagged.map((prod, index) => (
-                        <tr key={index}>
-                            <td>{prod.productName}</td>
-                            <td>{prod.avgRating}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            
-            <button className="add-to-bag-button">Add to my bag</button>
+            <div className="product-table">
+                <h2>Users who bagged this item also bagged</h2>
+                <table>
+                    <thead>  
+                    </thead>
+                    <tbody>
+                        {productInfo.usersAlsoBagged.slice(0, 5).map((prod, index) => (
+                            <tr key={index}>
+                                <td>{prod.productName}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
-}
-
+    
+}    
 export default ProductPage;
